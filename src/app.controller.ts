f145cd134b6b03b0f059f0cfca9716bd1f76ce7e
Tiroleza <fefetiroleza@gmail.com>
2024-06-03 00:43:49 -0300

    plate validation and register working

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,40 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { EstacionamentoService } from './app.service';
-import { CriarEstacionamentoDto } from './criar-estacionamento.dto';
+import { PlacaDto } from './dto/criar-estacionamento.dto';
 
-@Controller()
+@Controller('')
 export class EstacionamentoController {
   constructor(private readonly estacionamentoService: EstacionamentoService) {}
 
   @Post('/estacionamentos')
-  async criar(@Body() criarEstacionamentoDto: CriarEstacionamentoDto) {
-    console.log('Requisição recebida:', criarEstacionamentoDto);
-    return this.estacionamentoService.criarEstacionamento(
-      criarEstacionamentoDto.placa,
-    );
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async criar(@Body() criarEstacionamentoDto: PlacaDto) {
+    try {
+      const estacionamento =
+        await this.estacionamentoService.criarEstacionamento(
+          criarEstacionamentoDto.placa,
+        );
+      console.log('Placa registrada com sucesso:', estacionamento.placa);
+
+      return estacionamento;
+    } catch (error) {
+      // Tratamento de erros (opcional)
+      if (error instanceof HttpException) {
+        throw error;
+      } else {
+        throw new HttpException(
+          'Erro ao registrar a placa',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import { ValidationPipe } from '@nestjs/common'; // Import ValidationPipe from @nestjs/common package
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(bodyParser.json());
-  app.enableCors();
-
+  app.enableCors(); //por estar em duas portas diferentes, é necessário habilitar o CORS
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove propriedades não especificadas no DTO
+      forbidNonWhitelisted: true, // Retorna erro se houver propriedades não especificadas
+      transform: true, // Transforma o payload para o tipo especificado no DTO
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
